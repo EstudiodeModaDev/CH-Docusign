5347ce05e52038d4cfea74bc9eac8972e93d32cc
Daniel Palacios Vivero <dpalacios@estudiodemoda.com.co>
2026-01-23 15:11:11 -0500

    Envio de documentos

--- src/Components/GD/SendDocument/SendDocument.tsx
+++ src/Components/GD/SendDocument/SendDocument.tsx
@@ -109,7 +109,7 @@ export function mapNovedadToVM(n: Novedad): DocuSignVM {
   return {
     ...emptyVM(),
     nombre: n.NombreSeleccionado ?? "",
-    fechaIngreso: toISODateFlex(n.FECHA_x0020_REQUERIDA_x0020_PARA0) ?? "",
+    fechaIngreso: spDateToSpanishLong(n.FECHA_x0020_REQUERIDA_x0020_PARA0) ?? "",
     cargo: n.CARGO ?? "",
     ciudad: n.CIUDAD ?? "",
     conectividadLetras: n.auxconectividadtexto ?? "",
